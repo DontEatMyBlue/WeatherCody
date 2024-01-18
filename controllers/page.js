89f8fc0d304c75d7f484bada8e1fabf4d12dd143
DontEatMyBlue <simplehyun00@gmail.com>
2024-01-19 02:23:28 +0900

    Git Ignore 추가

--- controllers/page.js
+++ controllers/page.js
@@ -1,4 +1,19 @@
+const mysqlPool = require('../database/mysql_pool');
+
 exports.renderMain = (req,res,next)=>{
+    mysqlPool.getConnection((err,connection)=>{
+        if(err){
+            return res.status(500).json({error:'DB연동 문제 발생'})
+        }
+        connection.query('show databases',(error,results,fields)=>{
+            connection.release();
+            if(error){
+                console.log(error);
+            }
+            res.render("main");
+            console.log(results)
+        });
+
+    })
     console.log("현재 기온" + req.temp);
-    res.render("main");
 }
