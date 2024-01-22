ceab970a242baff244619d7acf02eab87e59ec6c
DontEatMyBlue <simplehyun00@gmail.com>
2024-01-22 21:59:29 +0900

    passport 구현

--- routes/page.js
+++ routes/page.js
@@ -5,5 +5,10 @@ const {getWeather} = require('../controllers/weather');
 const router = express.Router();
 
 router.get('/main',getWeather,renderMain);
-
+router.get('/join',(req,res)=>{
+    res.render('join');
+});
+router.get('/login',(req,res)=>{
+    res.render('login');
+});
 module.exports = router;
